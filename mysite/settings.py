8aada36962477910a88a2c96f7d968c51eb9926b
anah2run <anah2run@gmail.com>
2020-01-29 17:32:37 +0900

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9(0&nnhw2$do)_&g5#de7*8p+3q(mr0wu=r9g@=9q5so2b)(m0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
